63378a3a923ed8c6da4d91cb853686b4aff67f17
Techies Robotics <67329395+techiesrobotics@users.noreply.github.com>
2026-08-29 16:07:52 -0400

    Add SampleArm.py for motor control functions

---
+++ SampleArm.py
@@ -0,0 +1,23 @@
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port, Stop
+from pybricks.tools import wait
+
+# Initialize a motor on port E.
+
+left_arm = Motor(Port.E)
+ 
+def raiseArmUp(speed, angle):
+    left_arm.run_angle(speed, angle) 
+
+def lowerArmDown(speed, angle): 
+    left_arm.run_angle(speed, -1*angle)
+
+
+def testArm():
+    print ("from SampleArm main method")
+    raiseArmUp(100, 90)
+    lowerArmDown(100, 90)
+    
+   
+if __name__ == "__main__":
+    testArm()
