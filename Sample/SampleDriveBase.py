e3f42570baee6e267d790e4ed11a1d0966712238
Leo <leoleoleo11912@gmail.com>
2026-08-30 10:38:42 -0400

    Update SampleDriveBase.py

--- Sample/SampleDriveBase.py
+++ Sample/SampleDriveBase.py
@@ -1,9 +1,9 @@
-#############################################################
-#   For team Techies Robotics Teams training purpose only   #
-#           SampleDriveBase.py                              #
-#   This file is responsible for movement                   #
-# and should contain all the parts of the robot.            # 
-############################################
+################################################################
+#         Techies Robotics team training purposes only         #
+#   No sharing with or use by other teams without permission   #
+#        Contact techiesrobotics@gmail.com for permission      #
+################################################################
+
 #these are importsfor functions=============
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
